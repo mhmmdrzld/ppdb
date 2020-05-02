6d1da6ef37b27873207797a6200c2bf045c358fe
Muhammad Rizaldi <mrizaldi1998@gmail.com>
2020-05-02 18:38:03 +0800

    add index

---
+++ index.php
@@ -0,0 +1,41 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi PPDB</title>
+</head>
+
+<body>
+    <div>
+        <div>
+            <h1>Masuk</h1>
+        </div>
+        <div>
+            Silahkan <a href="masuk.php">LOGIN</a> pada form dibawah ini, atau <a href="daftar.php">DAFTAR</a> apabila belum mempunyai akun.
+        </div>
+        <div>
+            <form method="POST" >
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td>:</td>
+                    <td><input type="text" name="username"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>:</td>
+                    <td><input type="text" name="password"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><button type="submit" name="submit">Masuk</button></td>
+                </tr>
+            </table>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
